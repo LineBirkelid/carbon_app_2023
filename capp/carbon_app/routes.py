d1b8c4e6288da503a68fcaee3e60b812cdcd1bd4
LineBirkelid <126152136+LineBirkelid@users.noreply.github.com>
2023-02-28 10:36:48 +0100

    Update routes.py

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -7,9 +7,10 @@ def carbon_app_home():
     return render_template('carbon_app/carbon_app.html', title='carbon_app')
 
 @carbon_app.route('/carbon_app/new_entry')
-def new_entry():
+def new_entry_home():
     return render_template('carbon_app/new_entry.html', title='new_entry')
 
 @carbon_app.route('/carbon_app/your_data')
-def your_data():
+def your_data_home():
     return render_template('carbon_app/your_data.html', title='your_data')
+    
