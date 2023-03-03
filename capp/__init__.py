78da5cb515b834b9757bce89371d71601ed544f4
LineBirkelid <126152136+LineBirkelid@users.noreply.github.com>
2023-03-03 11:57:23 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -5,7 +5,6 @@ application = Flask(__name__)
 
 # application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 
-application.config['SECRET_KEY'] = '23b3436c2bd9f7ee888955a2611f160471f78097432f6d3d'
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
